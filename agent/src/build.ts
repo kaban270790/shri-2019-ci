78b8270c6a22a1399e19db85fe19c9ffcbbac7aa
d.timofeev <Dima27.07.90@mail.ru>
2019-10-13 18:43:39 +0300

    Агент найчился запускать команды

--- agent/src/build.ts
+++ agent/src/build.ts
@@ -1,16 +1,20 @@
 import request from "request";
+import {mkdirSync} from "fs";
+import {resolve as pathResolve} from "path";
+import {spawnSync,} from "child_process";
 
-let queue = [];
 let promiseQueue = (new Promise((resolve) => {
     resolve()
 }));
+
+const TMP_BUILD_DIR = pathResolve(__dirname, '..', 'tmp');
 
 export type BuildResultType = {
     id: number,
     repository: string,
     commit_hash: string,
     result: number,
-    command?: string,
+    command: string,
     stdout?: string,
     stderr?: string
 };
@@ -18,11 +22,12 @@ export type BuildResultType = {
 type ResultData = { id: number, status: number, stdout: string, stderr?: string };
 
 const sendResult = (data: ResultData) => {
+    console.log(data);
     request({
         method: 'POST',
         url: 'http://localhost:3000/notify_build_result', //todo из параметров
         json: data
-    }, (error, response) => {
+    }, (error) => {
         if (error) {
             console.error(error.toString());//todo возможно еще как-то уведомлять связи с сервером
         }
@@ -30,7 +35,7 @@ const sendResult = (data: ResultData) => {
 };
 
 export default function addToQueue(build: BuildResultType) {
-    promiseQueue.then(value => {
+    promiseQueue.then(() => {
         return createBuild(build)
             .then(data => {
                 sendResult(data);
@@ -42,13 +47,50 @@ export default function addToQueue(build: BuildResultType) {
 }
 
 const createBuild = (build: BuildResultType) => {
-    return (new Promise<ResultData>((res, rej) => {
-        setTimeout(() => {
-            res({
+    return (new Promise<ResultData>((resolve) => {
+            const cwd = pathResolve(TMP_BUILD_DIR, build.id.toString());
+            mkdirSync(cwd);
+            let result = {
                 id: build.id,
                 status: 0,
-                stdout: 'text'
+                stdout: '',
+                stderr: ''
+            };
+            let commands = build.command.split("\n");
+            commands.unshift(`git checkout ${build.commit_hash}`);
+            commands.unshift(`git clone ${build.repository} ./`);
+
+            let promiseExec = (new Promise<ResultData>((res) => {
+                res(result);
+            }));
+            promiseExec.then((result) => {
+                for (let i = 0, l = commands.length; i < l; i++) {
+                    let command = commands[i];
+                    let commandArray = command.split(" ");
+                    const file = commandArray.shift() || '';
+                    if (file.length > 0) {//todo надо изучить make и уйти от этой х-ни
+                        let {stderr, stdout, status} = spawnSync(file, commandArray, {cwd});
+                        let stdoutCommand = `\nCommand: ${command}\n`;
+                        if (status && status > 0) {
+                            result.status = status;
+                            result.stderr = (stdoutCommand + stderr);
+                            break;
+                        } else {
+                            result.stdout += (stdoutCommand + stdout);
+                        }
+                    }
+                }
+                return result;
             });
-        }, 10 * 1000);
-    }));
+
+            promiseExec.catch((stderr) => {
+                result.stderr = stderr;
+                resolve(result);
+            });
+            promiseExec.then((result) => {
+                resolve(result);
+            });
+        }
+    ));
 };
+
